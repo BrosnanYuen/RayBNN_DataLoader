17d206636a9055cfd494af2d008d694feaa26caf
brosnanyuen <brosnany@uvic.ca>
2023-09-02 20:35:24 -0700

    save

---
+++ tests/test_dataset.rs
@@ -0,0 +1,22 @@
+#![allow(unused_parens)]
+#![allow(non_snake_case)]
+
+use arrayfire;
+use RayBNN_DataLoader;
+
+const BACK_END: arrayfire::Backend = arrayfire::Backend::CUDA;
+const DEVICE: i32 = 0;
+
+
+
+
+#[test]
+fn test_dataset() {
+    arrayfire::set_backend(BACK_END);
+    arrayfire::set_device(DEVICE);
+
+
+
+
+
+}
