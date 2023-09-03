6de18391e022ceb5c3a15e36be9076e20ec7ae36
brosnanyuen <brosnany@uvic.ca>
2023-09-02 20:39:27 -0700

    save

--- tests/test_dataset.rs
+++ tests/test_dataset.rs
@@ -16,6 +16,12 @@ fn test_dataset() {
     arrayfire::set_device(DEVICE);
 
 
+    let inx_cpu: [f64; 5] = [0.0, -4.1, 1.7, -0.9, 0.3];
+
+    let instr: String = String::from("0.0, -4.1,1.7, -0.9, 0.3");
+    let outvec_cpu = RayBNN_DataLoader::Dataset::CSV::str_to_vec_cpu::<f64>(&instr);
+    assert_eq!(outvec_cpu, inx_cpu);
+
 
 
 
