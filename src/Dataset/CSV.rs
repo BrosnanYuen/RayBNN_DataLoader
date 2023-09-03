fc66f701d07998ce64978c5709ad2959ddea15b9
brosnanyuen <brosnany@uvic.ca>
2023-09-02 20:25:24 -0700

    save

--- src/Dataset/CSV.rs
+++ src/Dataset/CSV.rs
@@ -9,3 +9,32 @@ use std::fs;
 
 
 
+pub fn str_to_vec_cpu(
+	instr: &str
+) -> Vec<f64>  {
+
+	let mut vecf64: Vec<f64> = Vec::new();
+
+
+	let mut newline = instr.replace("\n", "");
+	newline = newline.replace(" ", "");
+
+	if newline.len() > 0
+	{
+		let strvec: Vec<&str> = newline.split(",").collect();
+		let ssize: u64 = strvec.len() as u64;
+
+		
+		for i in 0u64..ssize
+		{
+			let value:f64 = strvec[i as usize].parse::<f64>().unwrap();
+			vecf64.push(value);
+		}
+	}
+
+	vecf64
+}
+
+
+
+
