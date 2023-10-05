6fc2108c6ad80cee0639e07c954679d8f2221d5f
brosnanyuen <brosnany@uvic.ca>
2023-10-05 12:43:43 -0700

    save

--- src/Model/YAML.rs
+++ src/Model/YAML.rs
@@ -12,23 +12,36 @@ use std::fs::File;
 use std::io::Write;
 
 
-use nohash_hasher;
+
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 
 
-
-
-
-pub fn read<Z: arrayfire::RealFloating, V: arrayfire::Scanable>(
+pub fn read(
 	filename: &str,
 
     modeldata_string: &mut HashMap<String, String>,
-	modeldata_float: &mut HashMap<String, Z>,
-    modeldata_int: &mut HashMap<String, V>,
+	modeldata_float: &mut HashMap<String, f64>,
+    modeldata_int: &mut HashMap<String, u64>,
 	)
 {
 
-
+	if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(data) = line {
+                println!("{}", data);
+            }
+        }
+    }
 
 }
 
