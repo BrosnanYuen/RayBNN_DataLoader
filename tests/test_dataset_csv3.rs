23cd1013f1cedc16954e91c09799f5f4e734178f
brosnanyuen <brosnany@uvic.ca>
2023-10-30 15:31:10 -0700

    save

--- tests/test_dataset_csv3.rs
+++ tests/test_dataset_csv3.rs
@@ -14,12 +14,16 @@ use rand::{distributions::Standard, Rng};
 use std::collections::HashMap;
 
 #[test]
-fn test_dataset_csv2() {
+fn test_dataset_csv3() {
     arrayfire::set_backend(BACK_END);
     arrayfire::set_device(DEVICE);
 
 
+    let mut neuron_pos = RayBNN_DataLoader::Dataset::CSV::file_to_arrayfire::<f64>(
+    	"./test_data/neuron_pos3.csv",
+    );
 
 
+	arrayfire::print_gen("neuron_pos".to_string(), &neuron_pos,Some(6));
 
 }
