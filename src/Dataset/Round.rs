d996cfb29096ba57055bbef789a5399e20f13848
brosnan yuen <brosnanyuen@gmail.com>
2024-06-04 23:25:42 -0700

    save

--- src/Dataset/Round.rs
+++ src/Dataset/Round.rs
@@ -8,6 +8,35 @@ use rayon::prelude::*;
 
 
 
+const TEN: f64 = 10.0;
+
+pub fn rscalar(
+	input: f64,
+	decimal: u64
+	) -> f64  {
+
+	let places = TEN.powf(decimal as f64);
+	(input * places).round() / places
+}
+
+
+
+
+
+
+
+pub fn rvector(
+	input: &Vec<f64>,
+	decimal: u64
+	) -> Vec<f64>  {
+
+
+	input.par_iter().map(|&x|  rscalar(x , decimal) ).collect::<Vec<f64>>()
+}
+
+
+
+
 
 
 
