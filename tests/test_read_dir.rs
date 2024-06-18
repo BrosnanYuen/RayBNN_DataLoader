29a425d5013f2aaf350ac39e0a2e724adf16c5f7
brosnan yuen <brosnanyuen@gmail.com>
2024-06-17 20:10:48 -0700

    save

--- tests/test_read_dir.rs
+++ tests/test_read_dir.rs
@@ -15,7 +15,30 @@ use std::collections::HashMap;
 
 #[test]
 fn test_read_dir() {
+    arrayfire::set_backend(BACK_END);
+    arrayfire::set_device(DEVICE);
+    
+    let mut modeldata_string:  HashMap<String, String> = HashMap::new();
+    let mut modeldata_float:  HashMap<String, f64> = HashMap::new();
+    let mut modeldata_int:  HashMap<String, u64> = HashMap::new();
+    
 
-
+    RayBNN_DataLoader::Model::Misc::read_network_dir("./test_data/network_batch21/", 
+        modeldata_string, 
+        modeldata_float, 
+        modeldata_int, 
+        WValues, 
+        WRowIdxCSR, 
+        WColIdx, 
+        H, 
+        A, 
+        B, 
+        C, 
+        D, 
+        E, 
+        glia_pos, 
+        neuron_pos, 
+        neuron_idx
+    );
 
 }
