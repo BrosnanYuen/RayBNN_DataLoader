a23981492f8938e9f1ca7578b429802a68f345d8
brosnan yuen <brosnanyuen@gmail.com>
2024-06-17 19:23:28 -0700

    save

---
+++ tests/test_read_dir.rs
@@ -0,0 +1,21 @@
+#![allow(unused_parens)]
+#![allow(non_snake_case)]
+
+use arrayfire;
+use RayBNN_DataLoader;
+
+const BACK_END: arrayfire::Backend = arrayfire::Backend::CUDA;
+const DEVICE: i32 = 0;
+
+
+use rayon::prelude::*;
+
+use std::collections::HashMap;
+
+
+#[test]
+fn test_read_dir() {
+
+
+
+}
